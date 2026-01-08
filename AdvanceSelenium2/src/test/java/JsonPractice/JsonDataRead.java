6e8330a835faab2e591536da3953687cee82ed67
kiran2816 <pawarkiran199728@gmail.com>
2026-01-08 16:36:17 +0530

    added one line

--- AdvanceSelenium2/src/test/java/JsonPractice/JsonDataRead.java
+++ AdvanceSelenium2/src/test/java/JsonPractice/JsonDataRead.java
@@ -18,5 +18,6 @@ public void getDataFromJson() throws FileNotFoundException, IOException, ParseEx
 	Object object = p.parse(new FileReader("C:\\Users\\afzau\\OneDrive\\Desktop\\Important\\jsonData.json"));
 	JSONObject map=(JSONObject)object;
 	System.out.println(map.get("browser"));
+	System.out.println("one line");
 }
 }
